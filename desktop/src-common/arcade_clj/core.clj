326b80b6c8f98a7a9010cb8a54959924aca39c0b
shinmuro <shinmuro@gmail.com>
2016-05-24 21:35:46 +0900

    Screen の 切替

--- desktop/src-common/arcade_clj/core.clj
+++ desktop/src-common/arcade_clj/core.clj
@@ -2,6 +2,8 @@
   (:require [play-clj.core :refer :all]
             [play-clj.ui :refer :all]))
 
+(declare arcade-clj-game main-screen alt-screen)
+
 (defscreen main-screen
   :on-show
   (fn [screen entities]
@@ -11,11 +13,30 @@
   :on-key-down
   (fn [screen entities]
     (condp = (:key screen)
-      (key-code :escape) (app! :exit)))
+      (key-code :space) (set-screen! arcade-clj-game alt-screen)
+      (key-code :escape) (app! :exit)
+      entities))
   
   :on-render
   (fn [screen entities]
-    (clear!)
+    (clear! 1 0 0 1)
+    (render! screen entities)))
+
+(defscreen alt-screen
+  :on-show
+  (fn [screen entities]
+    (update! screen :renderer (stage)))
+
+  :on-key-down
+  (fn [screen entities]
+    (condp = (:key screen)
+      (key-code :space) (set-screen! arcade-clj-game main-screen)
+      (key-code :escape) (app! :exit)
+      entities))
+
+  :on-render
+  (fn [screen entities]
+    (clear! 0 1 0 1)
     (render! screen entities)))
 
 (defgame arcade-clj-game
